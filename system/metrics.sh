d3af7ecf859f07f0187a212f2c5c3dd1913010ee
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2025-05-26 20:28:13 +0200

    fix: better threshold control

--- system/metrics.sh
+++ system/metrics.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 THRESHOLD=80
+
 # Check CPU load
 CPU_LOAD=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 
 echo "CPU Load: $CPU_LOAD%"
+
+if [ $(echo "$CPU_LOAD > $THRESHOLD" | bc -l) -eq 1 ]; then
+    echo "Alert: CPU is above $THRESHOLD%"
+fi
+
 # Check memory usage
 MEMORY=$(free | awk '/Mem:/ {printf "%.2f", $3/$2 * 100.0}')
 
 echo "Memory Usage: $MEMORY%"
+
+if [ $(echo "$MEMORY > $THRESHOLD" | bc -l) -eq 1 ]; then
+    echo "Alert: MEMORY is above $THRESHOLD%"
+fi
+
 # Check disk space
 DISK=$(df -h | awk '$NF=="/"{print $(NF-1)}' | sed 's/%//')
 
 echo "Disk Usage: $DISK%"
-# Alert if any metric exceeds threshold
-if [[ "$CPU_LOAD" > "$THRESHOLD" || "$MEMORY" > "$THRESHOLD" || "$DISK" > "$THRESHOLD" ]]; then
-echo "Alert: One of the metrics is above $THRESHOLD%"
+
+if [ $(echo "$DISK > $THRESHOLD" | bc -l) -eq 1 ]; then
+    echo "Alert: DISK is above $THRESHOLD%"
 fi
